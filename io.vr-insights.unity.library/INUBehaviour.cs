6c960c5216bc939f55cff47e40aa74b7647d95c3
Javier Jerónimo <javier@jeronimosuarez.es>
2016-07-05 21:27:54 +0200

    First commit

---
+++ io.vr-insights.unity.library/INUBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace io.vr_insights.unity.library
+{
+    public interface INUBehaviour<D, A>
+        where D : INUBehaviourDetector
+        where A : INUBehaviourActivator
+    {
+
+    }
+
+    public interface INUBehaviourDetector
+    {
+        
+        void behaviourDetected(String type, String data);
+    }
+
+    public interface INUBehaviourActivator
+    {
+        void behaviourActivated(String type, String data);
+    }
+}
